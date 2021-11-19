01376f8a291434ed08beda05d0d0bd35e29d083d
sam bacha <sam@manifoldfinance.com>
2021-11-19 07:39:36 -0800

    dist abi

---
+++ graphgen_decerators.js
@@ -0,0 +1,11 @@
+/* @gg:source
+  name: Pair
+  onInit:
+    - UpdateField name
+    - UpdateField symbol
+    - UpdateField decimals
+    - UpdateField totalSupply
+    - UpdateField token0
+    - UpdateField token1
+    - UpdateField token1
+*/
